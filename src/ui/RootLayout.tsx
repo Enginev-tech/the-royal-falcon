9a5d585048eb2cbfe21d82906f3d48211c914512
Volker Willems <dev@volkerwillems.nl>
2026-01-27 09:43:10 +0000

    Fix anchor navigation for CTA links

--- src/ui/RootLayout.tsx
+++ src/ui/RootLayout.tsx
@@ -1,9 +1,35 @@
-import { Outlet, ScrollRestoration } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
 import { HeroVideo } from './components/HeroVideo'
 
 export function RootLayout() {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (!location.hash) {
+      return
+    }
+
+    const targetId = decodeURIComponent(location.hash.replace('#', ''))
+    const targetEl = document.getElementById(targetId)
+
+    if (targetEl) {
+      targetEl.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      return
+    }
+
+    const timeout = window.setTimeout(() => {
+      const retryEl = document.getElementById(targetId)
+      if (retryEl) {
+        retryEl.scrollIntoView({ behavior: 'smooth', block: 'start' })
+      }
+    }, 50)
+
+    return () => window.clearTimeout(timeout)
+  }, [location])
+
   return (
     <div>
       <Navbar />
